5884e322c4842172a34b70a16928bd390fe247e1
Artyom <artyom.kolyach@yandex.ru>
2025-12-23 15:27:13 +0300

    remove trash assets

--- src/app/config/routes.tsx
+++ src/app/config/routes.tsx
@@ -1,10 +1,4 @@
 import { type RouteObject } from "react-router";
-import { PaperPlane } from "../../pages/ui/subpages/PaperPlane.tsx";
-import { PoolPage } from "../../pages/ui/subpages/PoolPage.tsx";
-import { PineapplePage } from "../../pages/ui/subpages/PineapplePage.tsx";
-import { ShowPage } from "../../pages/ui/subpages/ShowPage.tsx";
-import { EightballPage } from "../../pages/ui/subpages/EightballPage.tsx";
-import { FinalPage } from "../../pages/ui/FinalPage.tsx";
 import { CarouselPage } from "@/pages/carousel/ui/CarouselPage.tsx";
 import { StartPage } from "@/pages/start-page/ui/StartPage.tsx";
 
@@ -19,30 +13,6 @@ export const routes: RouteObject[] = [
       {
         path: "results",
         element: <CarouselPage />,
-      },
-      {
-        path: "show",
-        element: <ShowPage />,
-      },
-      {
-        path: "next",
-        element: <EightballPage />,
-      },
-      {
-        path: "paper",
-        element: <PaperPlane />,
-      },
-      {
-        path: "deeper",
-        element: <PoolPage />,
-      },
-      {
-        path: "pineapple",
-        element: <PineapplePage />,
-      },
-      {
-        path: "/final",
-        element: <FinalPage />,
       },
     ],
   },
